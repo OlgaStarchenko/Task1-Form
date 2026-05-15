67a9a7c56f731c2394d3d146365e218dec3e00b4
OlgaStarchenko <o9915530@gmail.com>
2026-05-15 13:46:23 +1000

    The App component markup has been written, and the main styles have been defined in App.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import styles from "./App.module.css";
 
 export function App() {
-  return <p className={styles.text}>Hello!</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.error}>Ошибка</div>
+      <form className={styles.form}>
+        <input type="email" placeholder="Почта" />
+        <input type="password" placeholder="Пароль" />
+        <input type="password" placeholder="Повтор пароля" />
+        <button type="submit" className={styles.btn}>
+          Зарегистрироваться
+        </button>
+      </form>
+    </div>
+  );
 }
