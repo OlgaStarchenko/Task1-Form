c4a749887948cdbc2848efa9844a79967a2f86f6
OlgaStarchenko <o9915530@gmail.com>
2026-05-16 10:45:55 +1000

    I added states for input.value, created a formData object to store input.value, and wrote a sendData function to send the data. I added a value and onChange to each input. For form, I added onSubmit={sendData}

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 
 export function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const formData = {
+    email: email,
+    password: password,
+    repeatPassword: repeatPassword,
+  };
+  const sendData = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setEmail("");
+    setPassword("");
+    setRepeatPassword("");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.error}>Ошибка</div>
-      <form className={styles.form}>
-        <input type="email" placeholder="Почта" />
-        <input type="password" placeholder="Пароль" />
-        <input type="password" placeholder="Повтор пароля" />
+      <form className={styles.form} onSubmit={sendData}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(submit) => setEmail(submit.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChange={(submit) => setPassword(submit.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Повтор пароля"
+          value={repeatPassword}
+          onChange={(submit) => setRepeatPassword(submit.target.value)}
+        />
         <button type="submit" className={styles.btn}>
           Зарегистрироваться
         </button>
