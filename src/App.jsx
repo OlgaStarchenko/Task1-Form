0eaf9ad40c01cc565e7d37044eff6fa242caf8c5
OlgaStarchenko <o9915530@gmail.com>
2026-05-19 10:51:29 +1000

    Added the inputError state for displaying errors on the screen. The onEmailChange, onPasswordChange, and onRepeatPasswordChange functions handle errors.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export function App() {
     password: "",
     repeatPassword: "",
   });
+  const [inputError, setInputError] = useState(null);
 
   const { email, password, repeatPassword } = formData;
 
@@ -20,35 +21,72 @@ export function App() {
     });
   };
 
+  const onEmailChange = ({ target }) => {
+    setFormData({ ...formData, email: target.value });
+    let error = null;
+
+    if (target.value === "") {
+      error = "Введите адрес электронной почты";
+    }
+
+    setInputError(error);
+  };
+
+  const onPasswordChange = ({ target }) => {
+    setFormData({ ...formData, password: target.value });
+
+    let error = null;
+
+    if (target.value.length === "" || target.value.length < 6) {
+      error = "Пароль должен быть не менее 6 символов";
+    } else if (target.value.length > 10) {
+      error = "Пароль должен быть не более 10 символов";
+    }
+
+    setInputError(error);
+  };
+
+  const onRepeatPasswordChange = ({ target }) => {
+    setFormData({ ...formData, repeatPassword: target.value });
+
+    let error = null;
+
+    if (password !== target.value) {
+      error = "Пароли должны совпадать";
+    }
+
+    setInputError(error);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.error}>Ошибка</div>
+      {inputError && <div className={styles.error}>{inputError}</div>}
       <form className={styles.form} onSubmit={sendData}>
         <input
           type="email"
           placeholder="Email"
           value={email}
-          onChange={({ target }) => {
-            setFormData({ ...formData, email: target.value });
-          }}
+          onChange={onEmailChange}
         />
         <input
           type="password"
           placeholder="Пароль"
           value={password}
-          onChange={({ target }) => {
-            setFormData({ ...formData, password: target.value });
-          }}
+          onChange={onPasswordChange}
         />
         <input
           type="password"
           placeholder="Повтор пароля"
           value={repeatPassword}
-          onChange={({ target }) => {
-            setFormData({ ...formData, repeatPassword: target.value });
-          }}
+          onChange={onRepeatPasswordChange}
         />
-        <button type="submit" className={styles.btn}>
+        <button
+          type="submit"
+          className={styles.btn}
+          disabled={
+            inputError !== null || !email || !password || !repeatPassword
+          }
+        >
           Зарегистрироваться
         </button>
       </form>
